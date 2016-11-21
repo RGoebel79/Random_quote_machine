33373a603cfe9a4edff63ffd3c0833ff647a9066
RonG <racg550@gmail.com>
2016-11-21 17:36:27 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Random_quote_machine
+
+A website that gets quotes randomly from an API and displays them on screen. There is an option to Tweet the quote on twitter.
+
+Features an animation where a black 'curtain' covers the quote while it changes.
---
+++ index.js
@@ -0,0 +1,59 @@
+var newQuote = "";
+var newBy = "";
+
+function quote() {
+  $.ajax({
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous', // The URL to the API. 
+    headers: {
+      "X-Mashape-Key": "NrLzQQSJBzmshMUpwmeBa2spDc4up12TtKyjsnHD2I9NQWA62E",
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json"
+    },
+
+    success: function(data) {
+      var r = JSON.parse(data);
+      newQuote = r.quote;
+      newBy = r.author;
+
+    },
+    error: function(err) {
+      alert(err);
+
+    }
+  });
+}
+
+$(document).ready(function() {
+
+  $(".but1").on("click", function() {
+    quote();
+    $(".div1").animate({
+      height: "73%"
+    });
+    /*animation*/
+    $(".div1").text("Changing Quote...");
+
+    var myButton = $('.but1');
+    var myBox = $('.div1');
+
+    myBox.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+      function(e) {
+        $(".pquote").text('"' + newQuote + '"');
+        $(".by").text("- " + newBy);
+        $(".but2").attr('href', 'https://twitter.com/intent/tweet?text=' + '"' + newQuote + '" - ' + newBy);
+
+        myBox.animate({
+          height: "0px"
+        });
+        // code to execute after transition ends
+      });
+    myBox.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+      function(e) {
+
+        // code to execute after transition ends again
+        myBox.text("");
+        myButton.style("background-color: white");
+
+      });
+  });
+});
